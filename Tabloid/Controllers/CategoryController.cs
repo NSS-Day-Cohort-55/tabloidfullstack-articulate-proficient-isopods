1b67a404af6c2d00f9065ce69dee8e6aff0e6de2
ActuallyAJL <zero3260@hotmail.com>
2022-07-13 10:58:41 -0500

    adding categories works now

--- Tabloid/Controllers/CategoryController.cs
+++ Tabloid/Controllers/CategoryController.cs
@@ -20,5 +20,12 @@ namespace Tabloid.Controllers
         {
             return Ok(_categoryRepository.GetAll());
         }
+
+        [HttpPost]
+        public IActionResult Post(Category category)
+        {
+            _categoryRepository.Add(category);
+            return CreatedAtAction("Get", new { id = category.Id }, category);
+        }
     }
 }
--- Tabloid/client/src/components/category/CategoryList.js
+++ Tabloid/client/src/components/category/CategoryList.js
@@ -4,6 +4,10 @@ import { getAllCategories, addCategory } from "../../modules/categoryManager";
 
 export const CategoryList = () => {
   const [categories, setCategories] = useState([]);
+  const [isAddingCategory, setIsAddingCategory] = useState(false);
+  const [newCategory, setNewCategory] = useState({
+    name: "",
+  });
 
   const getCategories = () => {
     getAllCategories().then((r) => setCategories(r));
@@ -13,9 +17,69 @@ export const CategoryList = () => {
     getCategories();
   }, []);
 
+  const handleSubmitCategory = (event) => {
+    event.preventDefault();
+    addCategory(newCategory).then(() => setIsAddingCategory(false));
+  };
+
+  const handleFieldChange = (event) => {
+    const tempCategory = { ...newCategory };
+
+    let selectedTarget = event.target.value;
+
+    tempCategory[event.target.id] = selectedTarget;
+    setNewCategory(tempCategory);
+  };
+
+  const newCategoryCodeArray = [
+    <form className="new_review_form">
+      <h2>Add Category</h2>
+      <fieldset>
+        <label className="new_category_label" htmlFor="new_category_name">
+          Category
+        </label>
+        <input
+          type="text"
+          id="name"
+          onChange={handleFieldChange}
+          required
+          autoFocus
+          className="new_category_controlled_form"
+          placeholder="Category Name"
+          value={newCategory.name}
+        />
+      </fieldset>
+      <button
+        type="button"
+        id="category_add_submit_btn"
+        className="submit_btn"
+        onClick={handleSubmitCategory}
+      >
+        Submit
+      </button>
+      <button
+        type="button"
+        id="new_category_cancel_btn"
+        className="cancel_btn"
+        onClick={() => setIsAddingCategory(false)}
+      >
+        Cancel
+      </button>
+    </form>,
+    <button
+      type="button"
+      onClick={() => {
+        setIsAddingCategory(true);
+      }}
+    >
+      Add a New Category
+    </button>,
+  ];
+
   return (
     <div className="container">
       <div className="row justify-content-center">
+        {isAddingCategory ? newCategoryCodeArray[0] : newCategoryCodeArray[1]}
         {categories.map((category) => (
           <CategoryCard category={category} key={category.id} />
         ))}
