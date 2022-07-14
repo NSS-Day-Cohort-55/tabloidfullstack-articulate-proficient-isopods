73c1e3b1e1bcec89f472bb6a9d2b11c95759d447
ActuallyAJL <zero3260@hotmail.com>
2022-07-14 13:56:32 -0500

    merging

--- Tabloid/client/src/components/ApplicationViews.js
+++ Tabloid/client/src/components/ApplicationViews.js
@@ -4,7 +4,7 @@ import Login from "./Login";
 import Register from "./Register";
 import Hello from "./Hello";
 import { UserDetails } from "./Users/UserDetails";
-import {PostList} from "./post/PostList.js"
+import { PostList } from "./post/PostList.js";
 import { UserList } from "./Users/UserList";
 import { UpdateUser } from "./Users/UpdateUser";
 import { TagList } from "./tag/TagList";
@@ -13,7 +13,6 @@ import { TagEditForm } from "./tag/TagEditForm";
 import { PostDetail } from "./post/PostDetail";
 import { CategoryList } from "./category/CategoryList";
 import { PostEdit } from "./post/PostEdit";
-
 
 export default function ApplicationViews({ isLoggedIn }) {
   return (
@@ -25,17 +24,17 @@ export default function ApplicationViews({ isLoggedIn }) {
             element={isLoggedIn ? <PostList /> : <Navigate to="/login" />}
           />
           <Route path="tag" element={<TagList />} />
-          <Route path="/tag/add" element={<TagForm/>}/>
-          <Route path="/tag/:tagId/edit" element={<TagEditForm/>}/>
+          <Route path="/tag/add" element={<TagForm />} />
+          <Route path="/tag/:tagId/edit" element={<TagEditForm />} />
 
           <Route path="login" element={<Login />} />
           <Route path="register" element={<Register />} />
-          <Route path="users" element ={<UserList/>} />
-          <Route path="posts" element={<PostList/>}/>
-          <Route path="UserDetails/:userId" element={<UserDetails/>}/>
-          <Route path="EditUser/:userId" element={<UpdateUser/>}/>
-          <Route path="/:postId/PostDetails" element={<PostDetail/>}/>
-          <Route path="/:postId/Edit" element={<PostEdit/>}/>
+          <Route path="users" element={<UserList />} />
+          <Route path="posts" element={<PostList />} />
+          <Route path="UserDetails/:userId" element={<UserDetails />} />
+          <Route path="EditUser/:userId" element={<UpdateUser />} />
+          <Route path="/:postId/PostDetails" element={<PostDetail />} />
+          <Route path="/:postId/Edit" element={<PostEdit />} />
           <Route path="users" element={<UserList />} />
           <Route path="*" element={<p>Whoops, nothing here...</p>} />
           <Route path="category" element={<CategoryList />} />
